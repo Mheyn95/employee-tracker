074ddf249c57855594eafb57ddb96b1959626dbd
mheyn95 <mheyn95@gmail.com>
2021-02-06 13:54:48 -0700

    added credentials.env to keep password hidden

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const sql = require("mysql2");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
+require("dotenv").config();
 
 // create the connection to database
-const connection = sql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "employee_db",
+const connection = mysql.createConnection({
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
 });
