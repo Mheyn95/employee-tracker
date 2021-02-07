ec9ef4bf155d2de757abc95bcdc9abea2a05d4ca
mheyn95 <mheyn95@gmail.com>
2021-02-06 17:18:11 -0700

    initial queries working

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
+const { viewDept, viewRole, viewEmployee } = require("./lib/functions");
 require("dotenv").config({ path: __dirname + "/.env" });
 
 // create the connection to database
@@ -13,5 +15,51 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  initApp();
 });
+
+const question = {
+  type: "list",
+  name: "choice",
+  message: "What would you like to do?",
+  choices: [
+    "View All Departments",
+    "View All Roles",
+    "View All Employees",
+    "Add a Department",
+    "Add a Role",
+    "Add an Employee",
+    "Update an Employee Role",
+    // "Exit",
+  ],
+};
+
+function initApp() {
+  inquirer.prompt(question).then((answer) => {
+    switch (answer.choice) {
+      case "View All Departments":
+        viewDept(connection);
+        break;
+      case "View All Roles":
+        viewRole(connection);
+        break;
+      case "View All Employees":
+        viewEmployee(connection);
+        break;
+      case "Add a Department":
+        console.log("test");
+        break;
+      case "Add a Role":
+        console.log("test");
+        break;
+      case "Add an Employee":
+        console.log("test");
+        break;
+      case "Update an Employee Role":
+        console.log("test");
+      case "Exit":
+        process.exit();
+    }
+    initApp();
+  });
+}
